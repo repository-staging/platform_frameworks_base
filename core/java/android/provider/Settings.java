dd031d87458ec800ca3d3406727ddaece2703464
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-07-31 11:19:33 +0300

    Bluetooth auto turn off
    Co-authored-by: Pratyush <codelab@pratyush.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13507,6 +13507,10 @@ public final class Settings {
         @Protected(readWrite = KnownSystemPackage.SETTINGS)
         public static final String WIFI_AUTO_OFF = "wifi_off_timeout";
 
+        /** @hide */
+        @Protected(readWrite = KnownSystemPackage.SETTINGS)
+        public static final String BLUETOOTH_AUTO_OFF = "bluetooth_off_timeout";
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
