16995d28f49a5e0322d098251187303587f471fe
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-01-30 19:04:30 +0200

    disable package parser cache
    This is needed for properly verifying updates of system packages.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1722,7 +1722,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                         () -> LocalServices.getService(UserManagerInternal.class)),
                 (i, pm) -> new DisplayMetrics(),
                 (i, pm) -> new PackageParser2(pm.mSeparateProcesses, i.getDisplayMetrics(),
-                        new PackageCacher(pm.mCacheDir, pm.mPackageParserCallback),
+                        pm.hasCacheDir() ? new PackageCacher(pm.mCacheDir, pm.mPackageParserCallback) : null,
                         pm.mPackageParserCallback) /* scanningCachingPackageParserProducer */,
                 (i, pm) -> new PackageParser2(pm.mSeparateProcesses, i.getDisplayMetrics(), null,
                         pm.mPackageParserCallback) /* scanningPackageParserProducer */,
@@ -2280,8 +2280,14 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 t.traceEnd();
             }
 
-            mCacheDir = PackageManagerServiceUtils.preparePackageParserCache(
+            File cacheDir = PackageManagerServiceUtils.preparePackageParserCache(
                     mIsEngBuild, mIsUserDebugBuild, mIncrementalVersion);
+            if (cacheDir == null) {
+                // several places don't expect mCacheDir to be null, despite it being nullable
+                // upstream
+                cacheDir = new File("/dev/null");
+            }
+            mCacheDir = cacheDir;
 
             mInitialNonStoppedSystemPackages = mInjector.getSystemConfig()
                     .getInitialNonStoppedSystemPackages();
@@ -7976,6 +7982,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
 
     File getCacheDir() {
         return mCacheDir;
+    }
+
+    boolean hasCacheDir() {
+        return !"/dev/null".equals(mCacheDir.getPath());
     }
 
     PackageProperty getPackageProperty() {
