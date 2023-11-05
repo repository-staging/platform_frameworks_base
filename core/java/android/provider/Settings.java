6f54692931439bc124b42787e12b0c877ab9919a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-05 12:53:24 +0200

    add per-app memory tagging setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13529,6 +13529,10 @@ public final class Settings {
 
         /** @hide */
         @Protected(readWrite = KnownSystemPackage.SETTINGS)
+        public static final String FORCE_APP_MEMTAG_BY_DEFAULT = "force_app_memtag";
+
+        /** @hide */
+        @Protected(readWrite = KnownSystemPackage.SETTINGS)
         public static final String SHOW_SYSTEM_PROCESS_CRASH_NOTIFICATIONS = "show_system_process_crash_notifs";
 
         // ExtSettings END
