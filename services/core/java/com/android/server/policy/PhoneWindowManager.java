d6a2cd2bd62c2e92dcd9d5bb98fb14d1cee8285e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-19 17:21:43 +0300

    add setting for alternative touchscreen mode
    See the corresponding commit in device/google/tangorpro for more info.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5651,6 +5651,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (DEBUG_WAKEUP) Slog.i(TAG, "Display" + displayId + " turned off...");
 
         if (displayId == DEFAULT_DISPLAY) {
+            SystemProperties.set(
+                    android.ext.settings.AltTouchscreenMode.STATE_OF_DEFAULT_DISPLAY_PROP, "off");
             if (!isSwappingDisplay || mIsGoingToSleepDefaultDisplay
                     || !com.android.window.flags.Flags.skipSleepingWhenSwitchingDisplay()) {
                 updateScreenOffSleepToken(true /* acquire */, isSwappingDisplay);
@@ -5743,6 +5745,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     @Override
     public void screenTurnedOn(int displayId) {
         if (DEBUG_WAKEUP) Slog.i(TAG, "Display " + displayId + " turned on...");
+
+        if (displayId == DEFAULT_DISPLAY) {
+            SystemProperties.set(
+                    android.ext.settings.AltTouchscreenMode.STATE_OF_DEFAULT_DISPLAY_PROP, "on");
+        }
 
         reportScreenTurnedOnToWallpaper(displayId);
 
