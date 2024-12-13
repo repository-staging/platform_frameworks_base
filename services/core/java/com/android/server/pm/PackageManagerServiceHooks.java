3c0deab8bd4196515ee517a65d6b00f166b1cd80
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2024-12-13 13:03:45 +0000

    fixup! fixup! fixup! gosps: support for per-app GrapheneOS-specific persistent state

--- services/core/java/com/android/server/pm/PackageManagerServiceHooks.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceHooks.java
@@ -6,9 +6,11 @@ import android.annotation.UserIdInt;
 final class PackageManagerServiceHooks {
 
     static void systemReady(@NonNull PackageManagerService pm) {
+        GosPackageStatePmHooks.init(pm);
     }
 
     static void onClearApplicationUserData(
             @NonNull PackageManagerService pm, @NonNull String pkgName, @UserIdInt int userId) {
+        GosPackageStatePmHooks.onClearApplicationUserData(pm, pkgName, userId);
     }
 }
