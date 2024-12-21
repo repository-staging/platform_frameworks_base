3d402a9fc79e7a253cc5a43b94ef7dd91d5cb800
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2024-12-21 02:31:04 +0000

    SettingsProvider: support for additional checks for added settings downstream

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProviderHooks.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProviderHooks.java
@@ -1,7 +1,18 @@
 package com.android.providers.settings;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.ext.KnownSystemPackages;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Process;
 import android.os.UserHandle;
+import android.os.UserManager;
 import android.provider.Settings;
+
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
 
 class SettingsProviderHooks {
 
@@ -20,5 +31,103 @@ class SettingsProviderHooks {
      */
     private static void insertSetting(SettingsState state, String key, String value) {
         state.insertSettingLocked(key, value, null /* tag */, false /* makeDefault */, SettingsState.SYSTEM_PACKAGE_NAME);
+    }
+
+    static final int OPR_UNKNOWN = 0;
+    static final int OPR_READ_SETTING = 1;
+    static final int OPR_MUTATE_SETTING_INSERT = 2;
+    static final int OPR_MUTATE_SETTING_DELETE = 3;
+    static final int OPR_MUTATE_SETTING_UPDATE = 4;
+    static final int OPR_MUTATE_SETTING_RESET = 5;
+
+    static void maybeEnforceMorePermissions(
+            SettingsProvider settingsProvider, String name, String value, int settingsType,
+            int settingOperation, int callingUserId, int owningUserId,
+            Supplier<PackageInfo> callingPkgInfoSupplier) {
+        if (Binder.getCallingUid() == Process.ROOT_UID) {
+            return;
+        }
+
+        PackageInfo callingPkgInfo = callingPkgInfoSupplier.get();
+        final int callingUid = Binder.getCallingUid();
+        final int callingAppId = UserHandle.getAppId(callingUid);
+
+        ApplicationInfo callingAppInfo = callingPkgInfo.applicationInfo;
+        if (callingAppInfo == null) {
+            throw new SecurityException();
+        }
+
+        KnownSystemPackages ksp = KnownSystemPackages.get(settingsProvider.requireContext());
+
+        String callingPkgName = callingAppInfo.packageName;
+        switch (settingsType) {
+            case SettingsProvider.SETTINGS_TYPE_GLOBAL -> {
+                switch (name) {
+                    case Settings.Global.AUTO_REBOOT_TIMEOUT,
+                         Settings.Global.WIFI_AUTO_OFF,
+                         Settings.Global.BLUETOOTH_AUTO_OFF,
+                         Settings.Global.ALLOW_DISABLING_HARDENING_VIA_APP_COMPAT_CONFIG,
+                         Settings.Global.RESTRICT_MEMORY_DYN_CODE_LOADING_BY_DEFAULT,
+                         Settings.Global.RESTRICT_STORAGE_DYN_CODE_LOADING_BY_DEFAULT,
+                         Settings.Global.RESTRICT_WEBVIEW_DYN_CODE_LOADING_BY_DEFAULT,
+                         Settings.Global.FORCE_APP_MEMTAG_BY_DEFAULT,
+                         Settings.Global.SHOW_SYSTEM_PROCESS_CRASH_NOTIFICATIONS
+                            -> {
+                    }
+
+                    default -> {
+                        return;
+                    }
+                }
+            }
+
+            case SettingsProvider.SETTINGS_TYPE_SECURE -> {
+                switch (name) {
+                    case Settings.Secure.AUTO_GRANT_OTHER_SENSORS_PERMISSION
+                            -> {
+                    }
+                    case Settings.Secure.SCREENSHOT_TIMESTAMP_EXIF,
+                         Settings.Secure.SCRAMBLE_PIN_LAYOUT,
+                         Settings.Secure.SCRAMBLE_SIM_PIN_LAYOUT
+                            -> {
+                        switch (settingOperation) {
+                            case OPR_READ_SETTING -> {
+                                if (ksp.systemUi.equals(callingPkgName)
+                                        && callingAppInfo.isSystemApp()) {
+                                    return;
+                                }
+                            }
+                            default -> {
+                            }
+                        }
+                    }
+
+                    default -> {
+                        return;
+                    }
+                }
+            }
+
+            default -> {
+                return;
+            }
+        }
+
+        if (UserHandle.getAppId(callingAppInfo.uid) == Process.SYSTEM_UID
+                && callingAppInfo.isSystemApp()) {
+            if (SettingsState.SYSTEM_PACKAGE_NAME.equals(callingPkgName)
+                    || ksp.settings.equals(callingPkgName)) {
+                return;
+            } else {
+                throw new SecurityException();
+            }
+        }
+
+        if (callingAppId == Process.SHELL_UID
+                || (Build.IS_DEBUGGABLE && callingAppId == Process.ROOT_UID)) {
+            return;
+        }
+
+        throw new SecurityException("Not allowed to access additional write settings");
     }
 }
