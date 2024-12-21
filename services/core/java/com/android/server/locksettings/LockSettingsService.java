ed8ce1f5616d9222256e9a6d89f99a6c8eecf5ec
Fred Underwood <underwoodfred@proton.me>
2024-12-21 16:43:01 +1000

    add second factor to LockSettingsStateListener
    Change-Id: If9fde17bfc96dfb85ddf6685e54cbced011a67f5

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -2621,16 +2621,17 @@ public class LockSettingsService extends ILockSettings.Stub {
             }
         }
         final boolean success = response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK;
-        notifyLockSettingsStateListeners(success, userId);
+        notifyLockSettingsStateListeners(success, userId, lockDomain);
         return response;
     }
 
-    private void notifyLockSettingsStateListeners(boolean success, int userId) {
+    private void notifyLockSettingsStateListeners(boolean success, int userId,
+            LockDomain lockDomain) {
         for (LockSettingsStateListener listener : mLockSettingsStateListeners) {
             if (success) {
-                listener.onAuthenticationSucceeded(userId);
+                listener.onAuthenticationSucceeded(userId, lockDomain);
             } else {
-                listener.onAuthenticationFailed(userId);
+                listener.onAuthenticationFailed(userId, lockDomain);
             }
         }
     }
