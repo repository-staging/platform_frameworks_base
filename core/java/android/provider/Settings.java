3c11807d01d5fd599d5e068ea6eac90ce11e2087
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-29 18:56:46 +0200

    add battery charge limit setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13546,6 +13546,10 @@ public final class Settings {
         @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
         public static final String WIDEVINE_PROVISIONING_SERVER = "widevine_provisioner_server";
 
+        /** @hide */
+        @Protected(read = KnownSystemPackage.SYSTEM_UI, readWrite = KnownSystemPackage.SETTINGS)
+        public static final String BATTERY_CHARGE_LIMIT = "battery_charge_limit";
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
